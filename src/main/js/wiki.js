502032f69d1b38a26288f2841b6da0410b5bd667
Brian P. Holt <bholt@planetholt.com>
2013-07-07 19:08:25 -0500

    add tests for parser

--- src/main/js/wiki.js
+++ src/main/js/wiki.js
@@ -1,37 +1,52 @@
-(function($) {
-	var episodes = [];
+(function (global, $) {
+    "use strict";
 
-	function season(i,e) {
-		$(e).find("tr.vevent").each(episodeCurrier(i + 1));
-	}
+    $.extend({
+        interpolate: function(s, data) {
+            for(var variable in data) {
+                s = s.replace(new RegExp('{' + variable + '}','g'), data[variable]);
+            }
 
-	function episodeCurrier(seasonNumber) {
-		return function (i,e) { 
-			var $e = $(e),
-				episode = [],
-				cells = $e.find("td"),
-				airDate;
-			
-			episode.push("S" + zeroPad(seasonNumber) + "E" + zeroPad(i + 1));
-			episode.push($(cells[1]).text()); // title
-			episode.push($(cells[2]).text()); // director
-			episode.push($(cells[3]).text()); // writer
-			
-			episode.push(findDate($(cells[4]).text()));
-			
-			console.log(episode.join('*'));
-		};
-	}
+            return s;
+        }
+    });
 
-	function findDate(cellText) {
-		airDate = new Date(cellText);
-		return airDate.getFullYear() + "-" + (airDate.getMonth() + 1) + "-" + airDate.getDate();
-	}
-	
-	function zeroPad(n) {
-		return n < 10 ? "0" + n : n;
-	}
-	
-	$("table.wikitable.plainrowheaders").each(season);
-	
-}(jQuery));
+    function zeroPad(n) {
+        return n < 10 ? "0" + n : n;
+    }
+
+    function episodeToString() {
+        return $.interpolate('{seID}*{title}*{originalAirDate}*{productionCode}*{description}', this);
+    }
+
+    function episodeCurrier(seasonNumber) {
+        return function (i, e) {
+            var $e = $(e),
+                episode = {},
+                $cells = $e.find("th, td").map(function (i, e) { return $(e); });
+
+            episode.seID = "S" + zeroPad(seasonNumber) + "E" + zeroPad($cells[1].text());
+            episode.title = $cells[2].text().replace(/"/g, '');
+            episode.director = $cells[3].text();
+            episode.writer = $cells[4].text();
+            episode.originalAirDate = $cells[5].find('.bday').text();
+            episode.productionCode = $cells[6].text();
+            episode.description = $e.next().find(".description").text();
+            episode.toString = episodeToString;
+
+            return episode;
+        };
+    }
+
+    function season(i, e) {
+        var episodes = $(e).find("tr.vevent").map(episodeCurrier(i + 1));
+        return  episodes;
+    }
+
+    function init() {
+        var seasons = $("table.wikitable.plainrowheaders").map(season);
+        return  seasons;
+    }
+
+    global.wikiscrape = init;
+}(this, jQuery));
